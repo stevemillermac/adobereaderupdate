a7fc43f5427a1859096f794617238f1e9634bf74
Steve <stevemillermac@gmail.com>
2015-12-21 14:55:44 -0500

    Update AdobeReaderUpdate.sh

--- AdobeReaderUpdate.sh
+++ AdobeReaderUpdate.sh
@@ -16,15 +16,16 @@
 #   Version: 1.3
 #
 #   - v.1.0 Joe Farage, 23.01.2015
-#   - v.1.1 Joe Farage, 08.04.2015:   support for new Adobe Acrobat Reader DC
-#   - v.1.2 Steve Miller, 10.12.2015: umount and other minor fixes
-#	- v.1.3 Steve Miller, 16.12.2015: Updated to copy echo commands into JSS policy logs
+#   - v.1.1 Joe Farage, 08.04.2015 : support for new Adobe Acrobat Reader DC
+#   - v.1.2 Steve Miller, 10.12.2015 : umount and other minor fixes
+#	- v.1.3 Steve Miller, 16.12.2015	Updated to copy echo commands into JSS policy logs
+#	- v.1.4	Steve Miller, 21.12.2015:	Updated umount command to use hdiutil. 10.9 issues previous command
 #
 ####################################################################################################
 # Script to download and install Adobe Reader.
 # Only works on Intel systems.
 
-dmgfile="AcroRdrDC_${ARCurrVersNormalized}_MUI.dmg"
+
 logfile="/Library/Logs/AdobeReaderDCUpdateScript.log"
 
 # Are we running on Intel?
@@ -46,7 +47,7 @@ if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
     latestvernorm=`echo ${latestver}`
     # Get the version number of the currently-installed Adobe Reader, if any.
     if [ -e "/Applications/Adobe Acrobat Reader DC.app" ]; then
-        currentinstalledver=`/usr/bin/defaults read /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Info CFBundleShortVersionString`
+        currentinstalledver=`/usr/bin/defaults read /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Info.plist CFBundleShortVersionString`
         echo "Current installed version is: $currentinstalledver"
         if [ ${latestvernorm} = ${currentinstalledver} ]; then
             echo "Adobe Reader DC is current. Exiting"
@@ -59,6 +60,10 @@ if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
 
 
     ARCurrVersNormalized=$( echo $latestver | sed -e 's/[.]//g' )
+    dmgfile="AcroRdrDC_${ARCurrVersNormalized}_MUI.dmg"
+	dmgmount="AcroRdrDC_${ARCurrVersNormalized}_MUI"
+	
+	
     echo "ARCurrVersNormalized: $ARCurrVersNormalized"
     url1="http://ardownload.adobe.com/pub/adobe/reader/mac/AcrobatDC/${ARCurrVersNormalized}/AcroRdrDC_${ARCurrVersNormalized}_MUI.dmg"
     url2=""
@@ -81,26 +86,26 @@ if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
 
         /bin/sleep 10
         /bin/echo "`date`: Unmounting installer disk image." >> ${logfile}
-        /sbin/umount "/Volumes/AcroRdrDC_${ARCurrVersNormalized}_MUI"
+        diskutil unmount $dmgmount -quiet -force
         /bin/sleep 10
         /bin/echo "`date`: Deleting disk image." >> ${logfile}
         /bin/rm /tmp/${dmgfile}
 
         #double check to see if the new version got updated
-        newlyinstalledver=`/usr/bin/defaults read /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Info CFBundleShortVersionString`
+        newlyinstalledver=`/usr/bin/defaults read /Applications/Adobe\ Acrobat\ Reader\ DC.app/Contents/Info.plist CFBundleShortVersionString`
         if [ "${latestvernorm}" = "${newlyinstalledver}" ]; then
             /bin/echo "SUCCESS: Adobe Reader has been updated to version ${newlyinstalledver}"
             /bin/echo "`date`: SUCCESS: Adobe Reader has been updated to version ${newlyinstalledver}" >> ${logfile}
         else
         	/bin/echo "ERROR: Adobe Reader update unsuccessful, version remains at ${currentinstalledver}."
-          /bin/echo "`date`: ERROR: Adobe Reader update unsuccessful, version remains at ${currentinstalledver}." >> ${logfile}
-          /bin/echo "--" >> ${logfile}
-          exit 1
+            /bin/echo "`date`: ERROR: Adobe Reader update unsuccessful, version remains at ${currentinstalledver}." >> ${logfile}
+            /bin/echo "--" >> ${logfile}
+            exit 1
         fi
 
     # If Adobe Reader is up to date already, just log it and exit.       
     else
-        /bin/echo "Adobe Reader is already up to date, running ${currentinstalledver}."
+    	/bin/echo "Adobe Reader is already up to date, running ${currentinstalledver}."
         /bin/echo "`date`: Adobe Reader is already up to date, running ${currentinstalledver}." >> ${logfile}
         /bin/echo "--" >> ${logfile}
     fi  
